493cbca6e18af0bed10836053556446f06edea08
marinVcq <marinvacquier@hotmail.fr>
2023-10-12 20:40:22 +0200

    remove useless context

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Import context
-import { LanguageProvider } from './components/LanguageContext';
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LanguageProvider>
       <App />
-    </LanguageProvider>
   </React.StrictMode>
 );
